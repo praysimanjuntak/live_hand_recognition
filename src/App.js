7d42c73da9e81459de682998286dcf677e0686c0
Pray Apostel Simanjuntak <simanjuntakpray@gmail.com>
2022-03-03 23:59:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
+import { drawHand } from './utilities';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log('Handpose model loaded.');
+    // Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 100)
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const hand = await net.estimateHands(video);
+
+      const ctx = canvasRef.current.getContext('2d');
+      drawHand(hand, ctx);
+    }
+  }
+
+  runHandpose();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: '0',
+          right: '0',
+          textAlign: 'center',
+          zIndex: '9',
+          width: '640',
+          height: '480'
+        }} />
+        <canvas ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: '0',
+          right: '0',
+          textAlign: 'center',
+          zIndex: '9',
+          width: '640',
+          height: '480'
+        }} />
       </header>
+      <h1 style={{position: 'absolute', bottom: '0', width: '100%', background: 'white'}}>Please wait for 20 seconds for the program to run.</h1>
     </div>
   );
 }
